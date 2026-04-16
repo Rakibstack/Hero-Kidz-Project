0159c80eae894f569bd1afea487de564f7926d03
Rakibstack <rh6039511@gmail.com>
2026-04-16 23:18:00 +0600

    update footer section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans ">
      
-
+      <button>okay</button>
      <button className="btn btn-primary text-2xl p-10 rounded-2xl">click me</button>
     </div>
   );
