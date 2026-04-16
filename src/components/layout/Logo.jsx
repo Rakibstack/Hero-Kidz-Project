d834b3dfb05b5ef7ea3163d308dfafccf853eac6
Rakibstack <rh6039511@gmail.com>
2026-04-16 21:33:46 +0600

    design  hero kids logo

---
+++ src/components/layout/Logo.jsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <Link className='flex items-center gap-2' href={'/'}>
+            <Image
+            alt='logo-hero-kidzs'
+            src={'/assets/logo.png'}
+            width={60}
+            height={50}
+            />
+            <h2 className='text-2xl font-bold'>Hero <span className='text-secondary'>Kidz</span></h2>
+        </Link>
+    );
+};
+
+export default Logo;
