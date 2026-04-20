91b6ac3a760acfdbe71e9f672e1f4ad3f18fe4e4
Rakibstack <rh6039511@gmail.com>
2026-04-20 19:38:09 +0600

    update banner section

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,3 +1,4 @@
+import { FontBangla } from '@/app/layout';
 import Image from 'next/image';
 import React from 'react';
 
@@ -5,10 +6,10 @@ const Banner = () => {
 
     return (
         
-        <div className='flex justify-between items-center '>
+        <div className='flex justify-between items-center py-5'>
             <div className='flex-1 space-y-5'>
-                <h2 className='text-4xl font-bold'>আপনার শিশুকে একটি সুন্দর ভবিষ্যৎ দিন।</h2>
-                <p className='text-xl font-bold'>Buy Every Toy With Up TO 15% Discount</p>
+                <h2 className={`${FontBangla.className} text-6xl font-bold leading-18`}>আপনার শিশুকে দিন। একটি <span className='text-primary'>সুন্দর ভবিষ্যৎ</span> </h2>
+                <p className='text-xl font-medium'>Buy Every Toy With Up TO 15% Discount</p>
                 <button className='btn btn-primary btn-outline'>Explore More</button>
 
             </div>
