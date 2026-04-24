ac86aa42adcd1943609499959adb6d3d9ebf253b
Rakibstack <rh6039511@gmail.com>
2026-04-24 18:25:54 +0600

    add SEO for my application

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,8 +1,45 @@
 
 import ProductDetails from "@/components/cards/ProductDetails";
-import products from "../../data/toys.json";
 import { notFound } from "next/navigation";
 import { getSingleProduct } from "@/actions/server/product";
+
+export async function generateMetadata({ params }) {
+
+    const { id } = await params;
+
+
+ const product = await getSingleProduct(id);
+
+ if (!product) {
+   return {
+     title: "Product Not Found"
+   };
+ }
+
+ return {
+   title: product.title,
+
+   description: product.description.slice(0,160),
+
+   openGraph: {
+     title: product.title,
+     description: product.description.slice(0,160),
+
+     images: [
+       {
+         url: product.image,
+         width: 1200,
+         height: 630,
+       }
+     ],
+   },
+
+   twitter: {
+     card: "summary_large_image",
+     images: [product.image],
+   },
+ };
+}
 
 const ProductPage = async ({ params }) => {
   
