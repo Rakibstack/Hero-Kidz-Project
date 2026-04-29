9bdb4705fb6b872b7f1e9807c3842b0b879a8e23
Rakibstack <rh6039511@gmail.com>
2026-04-29 16:30:05 +0600

    handle google signIn and redirect

--- src/components/buttons/SocialLogin.jsx
+++ src/components/buttons/SocialLogin.jsx
@@ -1,26 +1,44 @@
-import { signIn } from 'next-auth/react';
-import React from 'react';
-import { FaGoogle } from 'react-icons/fa';
+import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+import { FaGoogle } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const SocialLogin = () => {
+  const param = useSearchParams();
 
-    const HandleSignIn = () => {
-        // Handle Google Sign-In logic here
-        const result = signIn('google', { redirect: false})
-        console.log(result);
-        
+  const HandleSignIn = async () => {
+    // Handle Google Sign-In logic here
+    const result = await signIn("google", {
+      redirect: false,
+      callbackUrl: param.get("callbackUrl") || "/",
+    });
+    console.log(result);
+    if (result.ok) {
+      Swal.fire({
+        icon: "success",
+        title: "Login Successful",
+        text: "You have been logged in successfully.",
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Login Failed",
+        text: "Google Sign-In failed. Please try again.",
+      });
     }
-
-
-    return (
-        <div>
-             <button onClick={HandleSignIn} className="btn btn-outline w-full mb-3 gap-2 rounded-xl">
-            <FaGoogle />
-            Continue with Google
-          </button>
-            
-        </div>
-    );
+  };
+  return (
+    <div>
+      <button
+        onClick={HandleSignIn}
+        className="btn btn-outline w-full mb-3 gap-2 rounded-xl"
+      >
+        <FaGoogle />
+        Continue with Google
+      </button>
+    </div>
+  );
 };
 
 export default SocialLogin;
